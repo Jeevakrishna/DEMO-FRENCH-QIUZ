6f7bd7969d8031be46dc4d704d58bf7e647b71e0
Jeeva Krishna <88268605+Jeevakrishna@users.noreply.github.com>
2021-08-19 20:10:36 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,158 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "des cambrioleurs sont entrés dans l'appartement de Vineetha.?",
+    answer: "La semaine dernière",
+    options: [
+      "La semaine dernière",
+      "Le mois dernier",
+      "L'année dernière",
+      "le dernier jour"
+    ]
+  },
+    {
+    numb: 2,
+    question: "............pleure sans cesse.?",
+    answer: "Vineetha",
+    options: [
+      "Shyamala",
+      "Vineetha",
+      "Sangeetha",
+      "Kumar"
+    ]
+  },
+    {
+    numb: 3,
+    question: ".se présente sur la scène de l'accident",
+    answer: "Un gendarme",
+    options: [
+      "Un gendarme",
+      "Un agent d'assurance",
+      "Un vieux homme",
+      "Un médecin"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Selon le témoin, C'est la faute du.........qui a fait l'accident?",
+    answer: "Chauffeur",
+    options: [
+      "Chauffeur",
+      "gendarme ",
+      "piéton ",
+      "mécanicien"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Ce jeune homme a grillé le feu ..............?",
+    answer: "rouge",
+    options: [
+      "Vert",
+      "rouge",
+      "jaune",
+      "gris"
+    ]
+  },
+
+   {
+    numb: 6,
+    question: "mange en lisant les messages sur son portable.?",
+    answer: "Vidya",
+    options: [
+      "Sanjay",
+      "Vidya",
+      "le fils",
+      "a belle – fille"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "On doit être plus prudent en ..............?",
+    answer: "Marchant",
+    options: [
+      "Marchant",
+      "jouant ",
+      "mangeant",
+      "courant"
+    ]
+  },
+
+ {
+    numb: 8,
+    question: "Ce sont............qui gâtent les enfants.?",
+    answer: "les parents",
+    options: [
+      "les grands-parents",
+      "les parents",
+      "les amis",
+      "les cousins"
+    ]
+  },
+
+ {
+    numb: 9,
+    question: "Elle a beaucoup .............?",
+    answer: "voyagé",
+    options: [
+      "voyager",
+      "voyagé",
+      "voyagée",
+      "voyage"
+    ]
+  },
+
+
+ {
+    numb: 10,
+    question: "Il n'y a plus............ gâteau.?",
+    answer: "de",
+    options: [
+      "du",
+      "de",
+      "de la",
+      "des"
+    ]
+  },
+
+ {
+    numb: 11,
+    question: "couleur voulez-vous essayer ?",
+    answer: "Quelle",
+    options: [
+      "Quel",
+      "Quels ",
+      "Quelle",
+      "Quelles"
+    ]
+  },
+
+ {
+    numb: 12,
+    question: "Saritha achète beaucoup ............romans français?.",
+    answer: "de",
+    options: [
+      "de",
+      "du ",
+      "de la",
+      "des"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
